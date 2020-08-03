235be2de10433cb490b80b0668eb64515ae5aaf3
Michael Overman <overman.michael@gmail.com>
2020-08-03 12:14:48 -0600

    feat: Beer Song
    Code written - sing the song, baby, sing it!!

--- kotlin/beer-song/src/main/kotlin/BeerSong.kt
+++ kotlin/beer-song/src/main/kotlin/BeerSong.kt
@@ -1,5 +1,14 @@
 object BeerSong {
-    fun verses(startBottles: Int, takeDown: Int){
-        TODO("Implement the function to complete the task")
-    }
+    fun verses(startBottles: Int, takeDown: Int) =
+        (startBottles downTo takeDown).map { n ->
+            "${bots(n, true).capitalize()}, ${bots(n, false)}.\n" +
+            "${if (n > 0) "Take ${if (n > 1) "one" else "it"} down and pass it around, ${bots(n - 1, true)}" else
+            "Go to the store and buy some more, ${bots(99, true)}"}.\n"
+        }.joinToString("\n")
+
+    private fun bots(n: Int, wall: Boolean) = when (n) {
+        1 -> "1 bottle"
+        0 -> "no more bottles"
+        else -> "$n bottles"
+    } + " of beer" + if (wall) " on the wall" else ""
 }
