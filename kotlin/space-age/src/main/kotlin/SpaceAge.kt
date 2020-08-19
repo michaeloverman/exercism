f681c57fd8516f7b8af90f5726ddc7f862f64c1d
Michael Overman <overman.michael@gmail.com>
2020-08-19 14:50:34 -0600

    feat: Space-Age
    Exercism task to calculate age in years on each planet.

--- kotlin/space-age/src/main/kotlin/SpaceAge.kt
+++ kotlin/space-age/src/main/kotlin/SpaceAge.kt
@@ -1,13 +1,13 @@
-class SpaceAge {
+fun Double.round(decimals: Int = 2): Double = "%.${decimals}f".format(this).toDouble() // found: https://discuss.kotlinlang.org/t/how-do-you-round-a-number-to-n-decimal-places/8843/4
 
-    // TODO: Implement proper constructor
-
-    fun onEarth(): Double = TODO("Implement this function to complete the task")
-    fun onMercury(): Double = TODO("Implement this function to complete the task")
-    fun onVenus(): Double = TODO("Implement this function to complete the task")
-    fun onMars(): Double = TODO("Implement this function to complete the task")
-    fun onJupiter(): Double = TODO("Implement this function to complete the task")
-    fun onSaturn(): Double = TODO("Implement this function to complete the task")
-    fun onUranus(): Double = TODO("Implement this function to complete the task")
-    fun onNeptune(): Double = TODO("Implement this function to complete the task")
+class SpaceAge(seconds: Long) {
+    private val years = seconds.toDouble() / 31557600
+    fun onEarth(): Double = years.round()
+    fun onMercury(): Double = (years / 0.2408467).round()
+    fun onVenus(): Double = (years / 0.61519726).round()
+    fun onMars(): Double = (years / 1.8808158).round()
+    fun onJupiter(): Double = (years / 11.862615).round()
+    fun onSaturn(): Double = (years / 29.447498).round()
+    fun onUranus(): Double = (years / 84.016846).round()
+    fun onNeptune(): Double = (years / 164.79132).round()
 }
